676a653e960a240c3d4c37492a17b2143a98b32d
aditya00123 <76775381+aditya00123@users.noreply.github.com>
2021-06-02 20:08:30 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,96 @@
+var dog,sadDog,happyDog, database;
+var foodS,foodStock;
+var addFood;
+var foodObj;
+
+//create feed and lastFed variable here
+var feed;
+var lastFed;
+
+var hour;
+
+function preload(){
+sadDog=loadImage("Dog.png");
+happyDog=loadImage("happy dog.png");
+}
+
+function setup() {
+  database=firebase.database();
+  createCanvas(1000,600);
+
+  foodObj = new Food();
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+  
+  dog=createSprite(800,200,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+
+  //create feed the dog button here
+  feed=createButton('Feed');
+  feed.position(725,95);
+  feed.mousePressed(feedDog);
+
+  addFood=createButton("Add Food");
+  addFood.position(800,95);
+  addFood.mousePressed(addFoods);
+
+  database.ref('/').set({
+   feedTime:hour(),
+  })
+
+ 
+}
+
+function draw() {
+  background(46,139,87);
+  foodObj.display();
+
+  //write code to read fedtime value from the database 
+  
+ 
+  //write code to display text lastFed time here
+
+ 
+  drawSprites();
+}
+
+//function to read food Stock
+function readStock(data){
+  foodS=data.val();
+  foodObj.updateFoodStock(foodS);
+}
+
+
+function feedDog(){
+  dog.addImage(happyDog);
+  if (foodS>=1)
+  foodS = foodS-1;
+  //write code here to update food stock and last fed time
+  database.ref('/').update({
+    Food:foodS,
+  })
+
+database.ref('feedTime').on('value',trans);
+
+if (hour < 12) {
+  text("Last Fed: "+hour+"am",625,95);
+}
+else {
+  text("Last Fed: "+hour-12+"pm",625,95)
+}
+
+}
+
+//function to add food in stock
+function addFoods(){
+  foodS++;
+  database.ref('/').update({
+    Food:foodS,
+  })
+}
+
+function trans(data) {
+hour = data.val();
+}
